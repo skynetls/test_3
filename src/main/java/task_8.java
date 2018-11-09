12776c2f45a5c985e107dc03055675242edcab36
skynetls <lyudocheksi@gmail.com>
2018-11-10 01:03:09 +0300

    Initial commit

---
+++ src/main/java/task_8.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class task_8 {
+    public static void main(String[] args) {
+        int mas[] = new int[5];
+        for (int i = 0; i < 5; i++) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Введите значение mas[" + i + "]");
+            mas[i] = scanner.nextInt();
+        }
+        System.out.println("Значение mas[] * 2: "+ mas [0]*2 +", "+ mas [1]*2 +", "+ mas [2]*2 +", "+ mas [3]*2 +", "+ mas [4]*2);
+
+    }
+}
