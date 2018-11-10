056b58f0abb47129ed4db6a4c10a760789923160
skynetls <lyudocheksi@gmail.com>
2018-11-11 02:45:03 +0300

    corrections

--- src/main/java/task_8.java
+++ src/main/java/task_8.java
@@ -3,14 +3,13 @@ import java.util.Scanner;
 public class task_8 {
     public static void main(String[] args) {
         int mas[] = new int[5];
+        Scanner scanner = new Scanner(System.in);
         for (int i = 0; i < 5; i++) {
-            Scanner scanner = new Scanner(System.in);
             System.out.println("Введите значение mas[" + i + "]");
             mas[i] = scanner.nextInt();
-            scanner.close();
         }
         System.out.println("Значение mas[] * 2: "+ mas [0]*2 +", "+ mas [1]*2 +", "+ mas [2]*2 +", "+ mas [3]*2 +", "+ mas [4]*2);
 
-
+        scanner.close();
     }
 }
--- src/main/java/task_9.java
+++ src/main/java/task_9.java
@@ -3,19 +3,18 @@ import java.util.Scanner;
 public class task_9 {
     public static void main(String[] args) {
         int table[][] = new int[2][3];
+        Scanner scanner = new Scanner(System.in);
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 3; j++) {
-                Scanner scanner = new Scanner(System.in);
                 System.out.println("Введите значение table[" + i + "][" + j + "]");
                 table[i][j] = scanner.nextInt();
-                scanner.close();
             }
-
         }
         for (int i = 0; i < 1; i++) {
             for (int j = 0; j < 3; j++) {
-                System.out.println("Значение table["+ i +"][" + j + "]: " + table[i][j]*3);
+                System.out.println("Значение table["+ i +"][" + j + "]*3: " + table[i][j]*3);
             }
         }
+        scanner.close();
     }
 }
